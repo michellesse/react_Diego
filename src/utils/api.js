38c1eeeed31b83c5645ee3e9ea6dfa306e846ec3
Brenda Michelle <bmichelle.psic@gmail.com>
2019-06-12 14:34:01 -0500

    error resuelto

--- src/utils/api.js
+++ src/utils/api.js
@@ -50,7 +50,7 @@ function patchData (path, id, data) {
     return fetch (
         `${API_URL}/${path}/${id}`,
         {
-            method: 'POST',
+            method: 'PATCH',
             headers : {
                 'Content-Type': 'application/json'
             },
